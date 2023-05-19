a649cf45bceae1b12e10c55c44a2295b884ce597
Novitskii_Kirill_Work <novitskii@pulsarm.ru>
2023-05-19 15:06:59 +0300

    Добавил главноую модель-представление

---
+++ WPF_BindablePasswordBox/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_BindablePasswordBox.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string? _password;
+
+        public string? Password 
+        { 
+            get => _password; 
+            set => SetProperty(value, ref _password);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool SetProperty<T>(T value, ref T field, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(value, field))
+            {
+                return false;
+            }
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
